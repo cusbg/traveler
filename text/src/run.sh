3410686e0c4a7ca3d2740d9ab49b5227b8c021e5
Richard Eliáš <richard.elias@matfyz.cz>
2015-12-18 00:14:41 +0100

    pridana LATEX template na bakalarku

---
+++ text/src/run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+TARGET="$1"
+OUTPUTDIR="$2"
+TARGET_TEX="${TARGET}.tex"
+
+fail_funct() {
+	echo "run failed"
+	exit 1
+}
+
+run_pdflatex() {
+	pdflatex -output-directory ${OUTPUTDIR} ${TARGET_TEX} \
+		|| fail_funct
+}
+run_bibtex() {
+	bibtex ${TARGET} \
+		|| fail_funct
+}
+
+run() {
+	run_pdflatex
+	run_bibtex
+	run_pdflatex
+	run_pdflatex
+}
+
+run
+
