646b72cabdf0a703b675f83e4a81b264d3be005e
hellb <david.hoksza@gmail.com>
2020-02-27 00:56:02 +0100

    parsing the numbering-label

--- src/utils/traveler_extractor.cpp
+++ src/utils/traveler_extractor.cpp
@@ -10,19 +10,22 @@ void traveler_extractor::extract(const string& filename)
     labels.clear();
     
     ifstream in(filename);
-    regex base_line("\\s*<point\\s+x=\"(.+)\"\\s+y=\"(.+)\"\\s+b=\"(.+)\"\\s*/>");
+    regex base_line("\\s*<point\\s+x=\"(.+)\"\\s+y=\"(.+)\"\\s+b=\"([^\"]+)\"\\s*(numbering-label=\"([^\"])\")?");
+//    regex base_line("\\s*<point\\s+x=\"(.+)\"\\s+y=\"(.+)\"\\s+b=\"(.+)\"\\s*/>");
     string line;
     smatch match;
     point p;
     string base;
+    string ix_label;
     
     while(getline(in, line))
     {
         if(regex_search(line, match, base_line))
         {
             stringstream s;
-            s << match[1] << " " << match[2] << " " << match[3];
-            s >> p.x >> p.y >> base;
+            // If the string does not include numbering label, match[5] results in an empty string and so does the ix_label
+            s << match[1] << " " << match[2] << " " << match[3] << " " << match[5];
+            s >> p.x >> p.y >> base >> ix_label;
             
             assert(!s.fail() && s.eof() && base.size() == 1);
             
