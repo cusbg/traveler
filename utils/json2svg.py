09c4cc18fb63558df81f2bcba854bbece4e6763f
David Hoksza <david.hoksza@gmail.com>
2022-05-20 10:43:34 +0200

    feat: json2svg - lines connecting subsequent residues in sequence

--- utils/json2svg.py
+++ utils/json2svg.py
@@ -11,24 +11,28 @@ class Point:
         self.x = x
         self.y = y
 
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
 
 class Dimensions:
     def __init__(self):
         self.p1 = Point(sys.maxsize, sys.maxsize)
         self.p2 = Point(-sys.maxsize, -sys.maxsize)
         
-    def update(self, x, y):
-        if x < self.p1.x:
-            self.p1.x = x
-        if y < self.p1.y:
-            self.p1.y = y
-        if x > self.p2.x:
-            self.p2.x = x
-        if y > self.p2.y:
-            self.p2.y = y
+    def update(self, p:Point):
+        if p.x < self.p1.x:
+            self.p1.x = p.x
+        if p.y < self.p1.y:
+            self.p1.y = p.y
+        if p.x > self.p2.x:
+            self.p2.x = p.x
+        if p.y > self.p2.y:
+            self.p2.y = p.y
 
 
 MARGIN = Point(10, 10)
+GRAY = 'rgb(204, 204, 204)'
 
 
 def error_exit(message):
@@ -79,7 +83,7 @@ def classes_defs():
         'fill': 'none'
     }, {
         'name': 'circle-gray',
-        'stroke': 'rgb(204, 204, 204)',
+        'stroke': GRAY,
         'fill': 'none'
     }, {
         'name': 'circle-brown',
@@ -90,10 +94,10 @@ def classes_defs():
         'stroke': 'rgb(0, 0, 0)'
     }, {
         'name': 'numbering-label',
-        'fill': 'rgb(204, 204, 204)'
+        'fill': GRAY
     }, {
         'name': 'numbering-line',
-        'stroke': 'rgb(204, 204, 204)'
+        'stroke': GRAY
     }, {
         'name': 'template',
         'visibility': 'hidden'
@@ -103,7 +107,10 @@ def classes_defs():
     }, {
         'name': 'residue-circle',
         'fill': 'rgb(255,255,255)'
-    }
+    }, {
+        'name': 'res-line',
+        'stroke': GRAY
+    },
     ]
 
 
@@ -137,20 +144,30 @@ def residues_to_svg(rna, dim: Dimensions, res_pos: Dict[int, Point], font_size):
 
     residues = '<g class="residues">'
 
+    # Lines connected residues in the sequence are drawn
+    p_prev: Point = None
     for res in rna['sequence']:
-        x = round(float(res['x']), 2) + MARGIN.x
-        y = round(float(res['y']), 2) + MARGIN.y
-        residues += f"<circle cx=\"{x}\" cy=\"{y}\" class=\"residue-circle\" r=\"{font_size*0.75}\"/>\n"
+        p = Point(round(float(res['x']), 2), round(float(res['y']), 2)) + MARGIN
+        if p_prev:
+            residues += f'<line x1="{p_prev.x}" y1="{p_prev.y}" x2="{p.x}" y2="{p.y}" class="res-line" />\n'
+        p_prev = p
+
+    # Circles forming the background of the residues (these need to come AFTER the connecting lines as they need to be "above" them and SVG does not have z-index)
     for res in rna['sequence']:
-        x = round(float(res['x']), 2) + MARGIN.x
-        y = round(float(res['y']), 2) + MARGIN.y
-        res_pos[res['residueIndex']] = Point(x, y)
-        dim.update(x, y)
+        p = Point(round(float(res['x']), 2), round(float(res['y']), 2)) + MARGIN
+        residues += f"<circle cx=\"{p.x}\" cy=\"{p.y}\" class=\"residue-circle\" r=\"{font_size*0.75}\"/>\n"
+
+    # Residue letters
+    for res in rna['sequence']:
+        p = Point(round(float(res['x']), 2), round(float(res['y']), 2)) + MARGIN
+        res_pos[res['residueIndex']] = p
+        dim.update(p)
         cls = " ".join(res['classes'])
         residues += f"<g>" \
                     f"<title>{res['residueIndex']} (position.label in template: {res['templateResidueIndex']}.{res['templateResidueName']}')</title>" \
-                    f"<text x=\"{x}\" y=\"{y}\" class=\"{cls}\" >{res['residueName']}</text>" \
+                    f"<text x=\"{p.x}\" y=\"{p.y}\" class=\"{cls}\" >{res['residueName']}</text>" \
                     f"</g>\n"
+
     residues += '</g>'
 
     return residues
@@ -178,17 +195,14 @@ def labels_to_svg(rna, dim: Dimensions):
         lbl_content = lbl['labelContent']
         lbl_line = lbl['labelLine']
         svg_labels += '<g class="label">\n'
-        x = lbl_content["x"] + MARGIN.x
-        y = lbl_content["y"] + MARGIN.y
-        x1 = lbl_line["x1"] + MARGIN.x
-        y1 = lbl_line["y1"] + MARGIN.y
-        x2 = lbl_line["x2"] + MARGIN.x
-        y2 = lbl_line["y2"] + MARGIN.y
-        dim.update(x, y)
-        dim.update(x1, y1)
-        dim.update(x2, y2)
-        svg_labels += f'<text x="{x}" y="{y}" class="{" ".join(lbl_content["classes"])}" >{lbl_content["label"]}</text>\n'
-        svg_labels += f'<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" class="{" ".join(lbl_line["classes"])}" />\n'
+        p = Point(lbl_content["x"],lbl_content["y"]) + MARGIN
+        p1 = Point(lbl_line["x1"], lbl_line["y1"]) + MARGIN
+        p2 = Point(lbl_line["x2"], lbl_line["y2"]) + MARGIN
+        dim.update(p)
+        dim.update(p1)
+        dim.update(p2)
+        svg_labels += f'<text x="{p.x}" y="{p.y}" class="{" ".join(lbl_content["classes"])}" >{lbl_content["label"]}</text>\n'
+        svg_labels += f'<line x1="{p1.x}" y1="{p1.y}" x2="{p2.x}" y2="{p2.y}" class="{" ".join(lbl_line["classes"])}" />\n'
         svg_labels += '</g>'
 
     svg_labels += '</g>\n'
